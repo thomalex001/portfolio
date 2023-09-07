91d83f1c19b27059293df4257837dbe41f3561f8
Alex Thomas MacBook <alexthomas@gmail.local>
2023-09-07 18:20:34 +0100

    deploy

--- src/components/common/SliderData.js
+++ src/components/common/SliderData.js
@@ -1,34 +1,31 @@
-// import Image1 from '../../assets/turntable.png';
+import Image1 from '../../assets/turntable.jpg';
 import Image2 from '../../assets/jazz-cafe.png';
 import Image3 from '../../assets/travel.png';
 import Image4 from '../../assets/bread.jpg';
-
-
+import Image5 from '../../assets/bike.png';
 
 export const SliderData = [
   {
-    // image: Image1,
+    image: 'Image1',
     alt: 'turntable'
   },
   {
-    image:
-      Image2,
+    image: Image2,
     alt: 'jazz-cafe'
   },
 
   {
-    image:
-      Image3,
+    image: Image3,
     alt: 'travel'
   },
 
   {
-    image:
-      Image4,
+    image: Image4,
     alt: 'bread'
-  }
+  },
 
-  // {
-  //   image: ''
-  // }
+  {
+    image: Image5,
+    alt: 'bike'
+  }
 ];
